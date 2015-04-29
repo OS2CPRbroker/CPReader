3e5b715390302b05556cfb571e4eb94e453fbf20
beemen <beshara_beemen@hotmail.com>
2015-04-29 16:07:18 +0200

    GUI now accepts 9 digit CPR numbers

--- public/javascripts/processQuery.js
+++ public/javascripts/processQuery.js
@@ -2,8 +2,8 @@ define(function () {
     return {
         processQuery: function (query, query2, online) {
 
-            var cpr = /\b[0-9]{10}$/; // a cpr consists of exactly 10 numbers
-            var cprpattern = /[0-9]{6}-[0-9]{4}$/; // or 10 numbers with a - between 6th and 7th char
+            var cpr = /\b[0-9]{9,10}$/; // a cpr consists of exactly 10 numbers
+            var cprpattern = /[0-9]{5,6}-[0-9]{4}$/; // or 10 numbers with a - between 6th and 7th char
             var lastname = /.*$/; // no commas
             var firstlastname = /.*,\s*.*$/; // one comma
             var firstmiddlelastname = /.*,\s*.*,\s.*$/; // two commas
@@ -13,7 +13,10 @@ define(function () {
 
             // if the input is recognised as a CPR number, redirect to a uuid search
             if (cpr.test(query) || cprpattern.test(query)) {
-                $.post('/search/cpr/', {"query": query.replace("-", "")}, function (data) {
+                query = query.replace("-", "");
+                if(query.length == 9)
+                    query = "0" + query;
+                $.post('/search/cpr/', {"query": query}, function (data) {
                     window.location = '/show/uuid/' + data + '/';
                 });
             }
