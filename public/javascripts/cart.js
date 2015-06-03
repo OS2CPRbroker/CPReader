162f95af24e6f527d33ec2c29013de1fb444c6bd
beemen <beshara_beemen@hotmail.com>
2015-06-03 15:11:34 +0200

    Code refactoring and enhancements: - Removed commented line - Cart copy now shows confirmation - Script removed from main.scala.html and put to cart.js

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -53,11 +53,55 @@
             );
         }
 
-        function showMessage(msgText){
-            var msgSpan= $('#message');
+        function showMessage(msgText, spnId){
+            if(!spnId)
+                spnId = 'message'
+            var msgSpan= $('#' + spnId);
             msgSpan.text(msgText);
         }
 
+        function getCPRNumbersString()
+        {
+            var table = document.getElementById("cart-table-rows");
+            var numbers="";
+
+            for (var r = 0, n = table.rows.length; r < n; r++)
+            {
+                var val = table.rows[r].cells[1].innerHTML;
+                numbers+= val + ',';
+            }
+            if(numbers.length > 0)
+                numbers = numbers.substr(0,numbers.length -1);
+            return numbers;
+        }
+
+        function copyCprNumbers(){
+            var numbers = getCPRNumbersString();
+            window.clipboardData.setData('text', numbers);
+            var txt = $('#spnCprNumbersCopied')
+            msg = txt.attr('txt');
+            showMessage(msg,'cartMessage');
+        }
+
+        function selectCPRNumbers(){
+            var numbers = getCPRNumbersString();
+            document.getElementById('copy-message').style.display = "block";
+            document.getElementById('cart-nums').innerHTML = numbers;
+            document.getElementById('cart-nums').style.display = "block";
+
+            if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined"){
+                var range = document.createRange();
+                range.selectNodeContents(document.getElementById('cart-nums'));
+                var sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+            else if (typeof document.selection != "undefined" && typeof document.body.createTextRange != "undefined"){
+                var textRange = document.body.createTextRange();
+                textRange.moveToElementText(document.getElementById('cart-nums'));
+                textRange.select();
+            }
+        }
 
         function showParents(event, uuid)
         {
