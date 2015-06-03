0861e189b0734543e8a2a081b7c5d75b38129d2c
beemen <beshara_beemen@hotmail.com>
2015-06-03 14:43:41 +0200

    Direct copy to clipboard in internet explorer

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -11,32 +11,4 @@
           url,'Cart','resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=no, width='+w+', height='+h+', top='+top+', left='+left);
       }
 
-      function selectCPRNumbers()
-      { 
-        document.getElementById('copy-message').style.display = "block";
 
-        var table = document.getElementById("cart-table-rows");
-        var numbers="";
-       
-        for (var r = 0, n = table.rows.length; r < n; r++)
-        {
-            numbers+=table.rows[r].cells[1].innerHTML
-            numbers+="<br>";
-        }
-        numbers+="<br>";
-
-        document.getElementById('cart-nums').innerHTML = numbers;
-        document.getElementById('cart-nums').style.display = "block";
- 
-        if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
-            var range = document.createRange();
-            range.selectNodeContents(document.getElementById('cart-nums'));
-            var sel = window.getSelection();
-            sel.removeAllRanges();
-            sel.addRange(range);
-        } else if (typeof document.selection != "undefined" && typeof document.body.createTextRange != "undefined") {
-            var textRange = document.body.createTextRange();
-            textRange.moveToElementText(document.getElementById('cart-nums'));
-            textRange.select();
-        }
-      }
