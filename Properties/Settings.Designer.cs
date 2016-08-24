55b1f38c51f182f90df3dcca550d86b0bb3b0c63
beemen <beshara_beemen@hotmail.com>
2016-08-24 14:06:52 +0200

    Settings file reorganization

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -61,15 +61,6 @@ namespace cpreader.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string accessfile_name {
-            get {
-                return ((string)(this["accessfile_name"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool accessfile_online {
             get {
@@ -89,15 +80,6 @@ namespace cpreader.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string cprbroker_applicationtoken {
-            get {
-                return ((string)(this["cprbroker_applicationtoken"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
         public string cprbroker_usertoken {
             get {
                 return ((string)(this["cprbroker_usertoken"]));
@@ -110,15 +92,6 @@ namespace cpreader.Properties {
         public int cprbroker_accesslevel {
             get {
                 return ((int)(this["cprbroker_accesslevel"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool cprbroker_fetchrelations {
-            get {
-                return ((bool)(this["cprbroker_fetchrelations"]));
             }
         }
         
@@ -164,6 +137,43 @@ namespace cpreader.Properties {
         public string cprbroker_domain {
             get {
                 return ((string)(this["cprbroker_domain"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\MagentaWorkspace\\CPReader\\aspnet-cpreader\\conf\\accessfile.config")]
+        public string accessfile_name {
+            get {
+                return ((string)(this["accessfile_name"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("bfb7c7dc-e4f3-4e20-ac86-a64fd0306e7a")]
+        public string cprbroker_applicationtoken {
+            get {
+                return ((string)(this["cprbroker_applicationtoken"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool cprbroker_fetchrelations {
+            get {
+                return ((bool)(this["cprbroker_fetchrelations"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
+        [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:1551/services/part.asmx")]
+        public string cpreader_PartService_Part {
+            get {
+                return ((string)(this["cpreader_PartService_Part"]));
             }
         }
     }
